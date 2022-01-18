1a917bf98da2115ad500a5160c7ebaef5af05e60
Grommester <90381005+Grommester@users.noreply.github.com>
2022-01-18 14:44:04 +0300

    Update main.cpp

--- OpenCV/task2/main.cpp
+++ OpenCV/task2/main.cpp
@@ -12,35 +12,6 @@
 #include "opencv2/highgui.hpp"
 #include <string>
 #include <cstring>
-
-
-//using namespace std;
-//char* data();
-//void file(char* dt);
-//int main()
-//{
-//	char* dt=data();
-//	file(dt);
-//	
-//}
-//char* data(){
-//	// текущая дата/время на основе текущей системы
-//	time_t now = time(0);
-//	// преобразовывает время в string form
-//	char* dt = ctime(&now);
-//	cout << "The local date and time is: " << dt << endl;
-//	return dt;
-//
-//}
-//void file(char* dt)
-//{
-//	std::ofstream file;
-//	file.open("test.txt", ios_base::app);
-//	unsigned long milliseconds_since_epoch = chrono::system_clock::now().time_since_epoch() / chrono::milliseconds(1);
-//	file << milliseconds_since_epoch <<" = " << dt << endl;
-//	file.close();
-//
-//}
 using namespace cv;
 using namespace std;
 
